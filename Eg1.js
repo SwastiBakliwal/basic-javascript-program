48d0fb85557d3c5fa0161201c92829123e8f29c0
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-28 23:23:06 +0530

    Add files via upload

---
+++ Eg1.js
@@ -0,0 +1,26 @@
+// console.log("Jai Jinendra");
+// console.log("namostu");
+// console.log(3+4)
+// console.log("3+4")
+
+// a = 10.3;
+// console.log(a+3);
+// x = "swastii";
+// console.log(x) ;
+//  b=5;
+//  console.log(b);
+//  b =b++;
+//  console.log(++b);
+
+
+age = 20 ;
+console.log("My age is "+age);
+ const a = 23;  //we can't change the value of const variable
+ //a++;
+ console.log(a)
+//in js variable can redefine
+///in js 
+var b = 5;
+console.log(b);
+const b = 3;  //will generate error bcoz of const 
+console.log(b);
