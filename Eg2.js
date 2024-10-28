b745e9c50a7f331eb7f3edb523555efa8ac316f0
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-28 23:23:31 +0530

    Add files via upload

---
+++ Eg2.js
@@ -0,0 +1,15 @@
+//operators in js
+let a = 15;
+let b = 6;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b); //a ki power b 
+console.log(a===b); //false
+console.log(a==b);  //false
+
+//Datatypes in js
+
