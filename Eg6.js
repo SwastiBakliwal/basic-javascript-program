25f6229b669e862f911d8cea35adc2d36d6445b9
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-10-29 21:11:33 +0530

    Add files via upload

---
+++ Eg6.js
@@ -0,0 +1,17 @@
+
+let firstName = "Swasti";
+let lastName = "Bakliwal";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName); // Output: "Swasti Bakliwal"
+
+
+let x = 10;
+let y = 20;
+
+console.log(x > y);  // Greater than: false
+console.log(x < y);  // Less than: true
+console.log(x >= y); // Greater than or equal to: false
+console.log(x <= y); // Less than or equal to: true
+console.log(x == y); // Equal to: false
+console.log(x != y); // Not equal to: true
