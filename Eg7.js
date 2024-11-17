e8af3e2883b5bb6f3b1a342c38d213e9e22f5a81
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-11-17 23:09:13 +0530

    Add files via upload

---
+++ Eg7.js
@@ -0,0 +1,17 @@
+console.log("Arrow function")
+const add = (a,b) => a+b;
+c=add(3,4);
+console.log(c);
+console.log("largest number");
+let largest =(i,j) => {
+    if(i>j){
+        console.log(i)
+    }
+    else{
+        console.log(j)
+    }
+} 
+k = largest(9,5) 
+
+//ques2
+  
